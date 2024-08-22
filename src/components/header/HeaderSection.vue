dd6a86b6cd9960d9dd482e649d6e7f9ac5a4d354
do.khanh.ly <do.khanh.ly@suns-Mac-mini-2.local>
2024-08-22 16:46:27 +0700

    feat(UI): header section
    fix
    
    url
    
    fix comment

---
+++ src/components/header/HeaderSection.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import './header.css' 
+import { defineComponent, ref } from 'vue'
+import { useI18n } from 'vue-i18n';
+import AlgoliaIcon from '../icons/IconAlgolia.vue'
+import SearchItems from '../search/SearchItems.vue';
+
+export default defineComponent({
+    components: {
+        AlgoliaIcon,
+        SearchItems
+    },
+    setup() {
+        const { t } = useI18n();
+        const searchQuery = ref('')
+        const placeholder = ref(t('searchPlaceholder'))
+
+        return {
+            searchQuery,
+            placeholder
+        }
+    }
+})
+</script>
+
+<template>
+    <header
+    class="header bg-transparent bg-cover bg-center bg-no-repeat bg-gradient-to-b from-yellow-400 to-orange-400 flex flex-col h-96 items-center justify-center px-4 py-2 text-center text-white"
+    >
+        <p class="header-logo">
+            <a :href="'https://algolia.com'" :aria-label="$t('headerLogoLabel')">
+                <AlgoliaIcon />
+            </a>
+        </p>
+        <p class="header-title text-4xl font-thin my-12">
+            {{ $t('headerTitle') }}
+        </p>
+        <div class="search-box h-16 w-4/5 max-w-[740px] relative">
+            <SearchItems :searchQuery="searchQuery" :placeholder="placeholder" />
+        </div>
+    </header>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: [],
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'hind': 'Hind, sans-serif',
+      }
+    },
   },
   plugins: [],
 }
